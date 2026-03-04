1c6c254d3da903c81509531fc7f43b4f13d6d261
Eli Cohavi <Eli.Cohavi@litigen.com>
2026-03-04 12:30:31 -0600

    Add project files.

---
+++ BarcodeSandbox/wwwroot/scanner.js
@@ -0,0 +1,8 @@
+﻿window.initKeyListener = function (dotNetRef) {
+    console.log("JS Loaded");
+
+    document.addEventListener("keydown", function (e) {
+        console.log("Key pressed:", e.key);
+        dotNetRef.invokeMethodAsync("ReceiveKey", e.key);
+    });
+};
